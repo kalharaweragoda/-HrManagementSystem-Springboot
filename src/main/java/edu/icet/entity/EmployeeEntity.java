a518b2696e883fe634e001afdd86e58ed25316b3
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-27 22:19:43 +0530

    EmployeeEntity create

---
+++ src/main/java/edu/icet/entity/EmployeeEntity.java
@@ -0,0 +1,46 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DepartmentType departmentType;
+
+    @Column(updatable = false)
+    private LocalDate createdAt;
+
+    private LocalDate updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDate.now();
+        this.updatedAt = LocalDate.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDate.now();
+    }
+}
