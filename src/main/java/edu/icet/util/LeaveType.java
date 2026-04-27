09606f0bb09f4604fee562572527ba67dc73e7fb
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-27 22:25:46 +0530

    LewveType Enum create

---
+++ src/main/java/edu/icet/util/LeaveType.java
@@ -0,0 +1,18 @@
+package edu.icet.util;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum LeaveType {
+    PATERNITY, UNPAID, CASUAL, MATERNITY, SICK, ANNUAL;
+
+    @JsonCreator
+    public static LeaveType fromString(String key) {
+        return LeaveType.valueOf(key.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
+}
