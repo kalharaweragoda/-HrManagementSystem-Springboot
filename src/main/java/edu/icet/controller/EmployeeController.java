082436290f82a752915504633410fd9731fcd94d
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-27 22:15:23 +0530

    Employee Controller annoration create

---
+++ src/main/java/edu/icet/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package edu.icet.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+@RequestMapping("/api/employee")
+public class EmployeeController {
+}
