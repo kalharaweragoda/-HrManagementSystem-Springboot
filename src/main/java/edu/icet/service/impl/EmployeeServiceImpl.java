2cb182699be614cafb9e07ecaa2497d88118895b
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-29 23:31:37 +0530

    create serviceImpl package

---
+++ src/main/java/edu/icet/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,11 @@
+package edu.icet.service.impl;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl {
+
+}
