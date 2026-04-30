7599e82bd91487957aef945604a135ec7026033d
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-30 22:06:31 +0530

    create EmployeeServiceImpl

--- src/main/java/edu/icet/service/EmployeeService.java
+++ src/main/java/edu/icet/service/EmployeeService.java
@@ -1,5 +1,13 @@
 package edu.icet.service;
 
 public interface EmployeeService {
+    List<LeaveSendDto> getAllLeaves();
 
+    LeaveSendDto getLeaveById(Integer id);
+
+    LeaveSendDto addLeave(LeaveDto leaveDto);
+
+    LeaveSendDto updateLeave(Integer id, LeaveDto leaveDto);
+
+    Boolean deleteLeave(Integer id);
 }
--- src/main/java/edu/icet/service/impl/EmployeeServiceImpl.java
+++ src/main/java/edu/icet/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,64 @@
 package edu.icet.service.impl;
 
 
+import edu.icet.dto.EmployeeDto;
+import edu.icet.entity.EmployeeEntity;
+import edu.icet.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceImpl {
 
+    private final EmployeeRepository employeeRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<EmployeeEntity> employeeRepositoryAll = employeeRepository.findAll();
+        List<EmployeeDto> employeeDtoList = new ArrayList<>();
+        employeeRepositoryAll.forEach(employeeEntity -> employeeDtoList.add(modelMapper.map(employeeEntity, EmployeeDto.class)));
+        return employeeDtoList;
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Integer id) {
+        if (employeeRepository.existsById(id)) {
+            EmployeeEntity employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
+            return modelMapper.map(employee, EmployeeDto.class);
+        }
+        return null;
+    }
+
+    @Override
+    public EmployeeDto addEmployee(EmployeeDto employeeDto) {
+        if (employeeDto.getId() == null) {
+            EmployeeEntity savedEntity = employeeRepository.save(modelMapper.map(employeeDto, EmployeeEntity.class));
+            return modelMapper.map(savedEntity, EmployeeDto.class);
+        }
+        return null;
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Integer id, EmployeeDto employeeDto) {
+        if (employeeRepository.existsById(id) && id.equals(employeeDto.getId())) {
+            EmployeeEntity savedEntity = employeeRepository.save(modelMapper.map(employeeDto, EmployeeEntity.class));
+            return modelMapper.map(savedEntity, EmployeeDto.class);
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean deleteEmployee(Integer id) {
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return !employeeRepository.existsById(id);
+        }
+        return false;
+    }
 }
