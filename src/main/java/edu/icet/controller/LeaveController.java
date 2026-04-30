d2a1529c72ff45c78feef3bf94393372f0f92211
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-30 22:09:59 +0530

    create LeaveController

---
+++ src/main/java/edu/icet/controller/LeaveController.java
@@ -0,0 +1,14 @@
+package edu.icet.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+@RequestMapping("api/leave")
+public class LeaveController {
+
+}
