3c96188da525ecb47ecaee4affa024a4b7931d9b
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-01 22:46:59 +0530

    create LeaveServiceImpl class

---
+++ src/main/java/edu/icet/service/impl/LeaveServiceImpl.java
@@ -0,0 +1,9 @@
+package edu.icet.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LeaveServiceImpl {
+}
