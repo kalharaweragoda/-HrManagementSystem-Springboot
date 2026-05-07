a14f938ba87a72bc1ac9fba05be19026016d5593
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-08 04:25:32 +0530

    create UserServiceImpl class

--- src/main/java/edu/icet/service/impl/UserServiceImpl.java
+++ src/main/java/edu/icet/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package edu.icet.service.impl;
 
+import edu.icet.dto.UserDto;
+import edu.icet.entity.UserEntity;
 import edu.icet.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -16,5 +18,30 @@ public class UserServiceImpl {
     private final ModelMapper modelMapper;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Override
+    public UserDto registerUser(UserDto userDTO) {
+        if (userRepository.existsByUsername(userDTO.getUsername())) {
+            return null;
+        }
 
+        UserEntity userEntity = modelMapper.map(userDTO, UserEntity.class);
+        userEntity.setPassword(bCryptPasswordEncoder.encode(userDTO.getPassword()));
+
+        UserEntity savedUserEntity = userRepository.save(userEntity);
+        UserDto savedUserDto = modelMapper.map(savedUserEntity, UserDto.class);
+        savedUserDto.setPassword(null);
+
+        return savedUserDto;
+    }
+
+    @Override
+    public UserDto getUserByUsername(String username) {
+        if (!userRepository.existsByUsername(username)) {
+            return null;
+        }
+        UserEntity userEntity = userRepository.findByUsername(username);
+        UserDto userDTO = modelMapper.map(userEntity, UserDto.class);
+        userDTO.setPassword(null);
+        return userDTO;
+    }
 }
