a0b749c65d74b7b1fc826d4c52fb82ffb12bfbf2
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-08 04:24:25 +0530

    create UserServiceImpl variable

--- src/main/java/edu/icet/service/impl/UserServiceImpl.java
+++ src/main/java/edu/icet/service/impl/UserServiceImpl.java
@@ -1,11 +1,20 @@
 package edu.icet.service.impl;
 
+import edu.icet.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
 }
